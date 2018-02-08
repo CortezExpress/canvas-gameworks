f01d434f55903e88e13f33cee5b9008f421e0b2b
MARTIN GENET <martingenet@MHG.local>
2018-02-07 19:12:21 -0500

    just some game play touch ups

--- canvas.js
+++ canvas.js
@@ -12,7 +12,9 @@ var score1 = 0;
 var score2 = 0;
 var chickenDinner = 3;
 var winScreen = false;
-var is2Playas = false
+// var game = setTimeout(gameLoop, 1000/30);
+// tempBallSpeedX = 0;
+// tempBallSpeedY =0;
 
 // mouse movement
 function calculateMousePos (evt){
@@ -77,36 +79,7 @@ document.getElementById('start-game-button').onclick = function () {
 		
 		
 }
-////// for two players
-// document.getElementById('two-play-button').onclick = function () {
-// 		canvas = document.getElementById('gameCanvas');		
-// 		ctx = canvas.getContext('2d');
 
-// 		document.getElementById('title').style.display = 'none';
-// 		// document.getElementById('gameDiv').style.display = 'block';
-
-		
-// 		var framesPerSecond = 30;
-// 		setInterval(function (){
-// 			moveEverything2PLayers();
-// 			drawEverything();
-// 		}, 1000/framesPerSecond);
-
-// 		canvas.addEventListener('mousemove', function (evt) {
-// 			var mousePos = calculateMousePos(evt);
-// 			stick1Y = mousePos.y-(paddleHeight*0.5);
-
-// 		canvas.addEventListener('mousedown', restartClick)
-
-// 		});
-		
-		
-// }
-	// function callBoth(){
-	// 	moveEverything();
-	// 	drawEverything();
-
-	// }
 
 
 	function AIBaby(){
@@ -121,15 +94,7 @@ document.getElementById('start-game-button').onclick = function () {
 
 	// bodies in motion
 	function moveEverything(){
-// 	document.addEventListener('keydown', function (evt){
-// 	var keyPress = String.fromCharCode(evt.keyCode);
-// 	if(keyPress == "L"){
-// 		stick2Y += 15;
-// 	} else if(keyPress == "P"){
-// 		stick2Y -= 15;
-// 	}
-// });
-		
+
 		// Capturing the value of the selected radio button
 		var gamePlay = document.querySelector('input[name="cuantosPlayas"]:checked').value;
 
@@ -140,7 +105,7 @@ document.getElementById('start-game-button').onclick = function () {
 		ballX += ballSpeedX;
 		bally += ballSpeedY;
 			
-		if (ballX < 0){
+		if (ballX < 34){
 
 //need to know if ball is above the bottom and below the top of the paddle 
 // we add paddleHeight to get the bottom because Y goes down as it increases
@@ -155,7 +120,7 @@ document.getElementById('start-game-button').onclick = function () {
 				ballReset();
 			}
 		}
-		if (ballX > canvas.width){
+		if (ballX > canvas.width-34){
 			if( bally > stick2Y && bally < stick2Y + paddleHeight) {
 				ballSpeedX = - ballSpeedX;
 				var deltaY = bally-(stick2Y + paddleHeight/2);
@@ -168,62 +133,14 @@ document.getElementById('start-game-button').onclick = function () {
 
 // below is to make sure the ball doesn't go above or below the ceiling or floor. 
 		if (bally<0){
-			ballSpeedY = -ballSpeedY;
+			ballSpeedY *= -1;
 		}
 		if (bally > canvas.height){
-			ballSpeedY = -ballSpeedY;
+			ballSpeedY *= -1;
 		}
 
 	}
 	
-
-///////let's try again for 2
-// function moveEverything2PLayers(){
-
-		// AIBaby();
-		// ballX += ballSpeedX;
-		// bally += ballSpeedY;
-			
-		// if (ballX < 0){
-
-//need to know if ball is above the bottom and below the top of the paddle 
-// we add paddleHeight to get the bottom because Y goes down as it increases
-// if ball hits below zero and is not between the the paddle Y then score, check winner, and reset.
-// 			if( bally > stick1Y && bally < stick1Y + paddleHeight) {
-// 				ballSpeedX = - ballSpeedX;
-
-// 					var deltaY = bally -(stick1Y + paddleHeight/2);
-// 					ballSpeedY =deltaY * 0.35;
-// 			} else{
-// 				score2 ++;
-// 				ballReset();
-// 			}
-// 		}
-// 		if (ballX > canvas.width){
-// 			if( bally > stick2Y && bally < stick2Y + paddleHeight) {
-// 				ballSpeedX = - ballSpeedX;
-// 				var deltaY = bally-(stick2Y + paddleHeight/2);
-// 					ballSpeedY =deltaY * 0.35;
-// 			} else{
-// 				score1 ++;
-// 				ballReset();
-// 			}
-// 		}
-
-// // below is to make sure the ball doesn't go above or below the ceiling or floor. 
-// 		if (bally<0){
-// 			ballSpeedY = -ballSpeedY;
-// 		}
-// 		if (bally > canvas.height){
-// 			ballSpeedY = -ballSpeedY;
-// 		}
-
-// 	}
-
-
-
-
-
 	function drawCenterLine () {
 		for (var i=0;i<canvas.height; i+=40){
 // -1 on the x coordinate because 2px width line so push the line off-center by half the width
@@ -260,9 +177,9 @@ document.getElementById('start-game-button').onclick = function () {
 				
 
 		// left side paddle - player 1 - mouse
-		drawBox(0, stick1Y, paddleWidth, paddleHeight, 'white');
+		drawBox(20, stick1Y, paddleWidth, paddleHeight, 'white');
 		// Right side paddle - player 2 -subtract the width of the paddle
-		drawBox(canvas.width-paddleWidth, stick2Y, paddleWidth, paddle2Height, 'white');
+		drawBox(canvas.width-paddleWidth-20, stick2Y, paddleWidth, paddle2Height, 'white');
 		
 		ctx.fillText(score1, 100, 100);
 		ctx.fillText(score2, canvas.width-100, 100);
@@ -303,51 +220,69 @@ function ballReset (){
 }
 
 
-
-// function moveArrows(e){
-// 	// alert(e.keyCode)
-// // 38 = up arrow key
-// 	if (e.keyCode = 38){
-// 		stick2Y +=6;
+// function keyDown(e){
+// 	if (e.keyCode == 32) {
+// 		pauseGame();
 // 	}
-// // 40 = down arrow key
-// 	if (e.keyCode = 40){
-// 		stick2Y-= 6;
-// 	}
-// 	canvas.height=canvas.height;
-// 	ctx.
 // }
 
-// document.onkeydown = moveArrows;
-// function makePlayerTwoWork (){
-// 
-
-
-
-
+// function pauseGame() {
+// 	game = 
+// }
 
 // document.addEventListener('keydown', function (evt){
+// 		var keyPress = String.fromCharCode(evt.keyCode);
+// 		if(keyPress == "G"){
+// 			clearInterval();
+// 		}
+
+
+
+
+
+
+
+	document.addEventListener('keydown', function (evt){
+	var keyPress = String.fromCharCode(evt.keyCode);
+	var tempBallSpeedX = 0;
+	var tempBallSpeedY =0;
+	if(keyPress == "G"){
+		if (ballSpeedX!=0){
+		tempBallSpeedX = ballSpeedX ;
+		tempBallSpeedY = ballSpeedY;
+		ballSpeedX = 0;
+		ballSpeedY = 0;
+		} else {
+			ballSpeedX = tempBallSpeedX;
+			ballSpeedY = tempBallSpeedY;
+		}
+	}
+});
+	
+
+
+
+
+
+// unfruitful attempt to create a pause ability
+// document.addEventListener('keydown', function (evt){
 // 	var keyPress = String.fromCharCode(evt.keyCode);
-// 	if(keyPress == "L"){
-// 		stick2Y += 15;
-// 	} else if(keyPress == "P"){
-// 		stick2Y -= 15;
+	
+// 	if (ballSpeedX!=0){
+// 	if(keyPress == "G"){
+// 		tempBallSpeedX = ballSpeedX ;
+// 		tempBallSpeedY = ballSpeedY;
+// 		ballSpeedX = 0;
+// 		ballSpeedY = 0;
+// 	}
+// 	}
+// 	if (ballSpeedX=0){
+// 		if(keyPress == "G"){
+// 			ballSpeedX = tempBallSpeedX;
+// 			ballSpeedY = tempBallSpeedY;
+// 	}
 // 	}
 // });
 
-
-// good morning. i have a new question, but it's more about strategy of solving a problem (as opposed to 
-// something not working).  i finally got my right side paddle working with keys this morning. i haven't 
-// tried the arrow keys bc i first wanted it to work with letters (the left paddle is controlled by the 
-// mouse). right now my right side key control is a "document.addEventListener('keydown', f(x))" and 
-// is not inside a function.  my gut is to put either a radio (requires just one choice) above my start 
-// button that allows the user to choose 1 or 2 players. then i will have a function that runs on an if 
-// else statement on whether to use automated computer movement (for 1 player) or keys work (for 2 players).
-//  my real reason for asking is that between you, in class, and tutorials i try to watch at night and in 
-// the morning, i feel "the professionals" would make a variable (var is2players = false) and then toggle 
-// or switch it to true so that the function being run is one of those easy ones from the hangman where you 
-// nly need to switch the variable to true (from false). for the life of me i can't see why that is easier,
-// but i feel like that is how you would advise to approach it. i therefore wanted to know if i am correct 
-// and if i should just do it your way because it will be more helpful to me in the future when things get 
-// crazier? or maybe i am missing something? if you prefer we can do this in person, but maybe because 
-// students want/need more personal attention in class you prefer to just reply in slack. 
+// var ballSpeedX = 10;
+// var ballSpeedY = 4;
